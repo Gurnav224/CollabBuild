e8f754ebf84448bd709d3bba4e69737402b48efe
Gurnav224 <chaudharyg856@gmail.com>
2024-03-18 22:04:04 +0545

    solve db issue

--- backend/app.js
+++ backend/app.js
@@ -50,6 +50,26 @@ app.get('/test',async(req,res)=>{
     }
 })
 
+app.post('/users', async (req, res) => {
+    try {
+      const { username, email, password } = req.body;
+  
+      // Check if the user already exists
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+  
+      // Create a new user
+      const newUser = new User({ username, email, password });
+      const savedUser = await newUser.save();
+  
+      res.status(201).json(savedUser);
+    } catch (error) {
+      res.status(500).json({ error: 'Error occurred', details: error.message });
+    }
+  });
+
 app.get('/users',async(req,res)=>{
     try {
         const users = await User.find()
--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://127.0.0.1:27017/project-management-DB")
+
+const uri = "mongodb+srv://chaudharyg856:IRI1eqBdKCgbkY5E@cluster0.0konzrv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(uri)
 
 
 const db = mongoose.connection;
